1935db0500f9b68eac0b029f9fe4e04bfedc2a57
qwerty <qwerty@pilsfree.net>
2017-11-26 17:57:32 +0100

    ddl

---
+++ db/ddl.sql
@@ -0,0 +1,38 @@
+
+
+PRAGMA writable_schema = 1;
+delete from sqlite_master where type in ('table', 'index', 'trigger');
+PRAGMA writable_schema = 0;
+
+
+CREATE TABLE uzivatele (
+  iduzivatel integer PRIMARY KEY AUTOINCREMENT,
+  jmeno varchar,
+  login varchar,
+  heslo varchar,
+  email varchar,
+  idprava integer
+);
+
+CREATE TABLE prava (
+  idprava integer PRIMARY KEY AUTOINCREMENT,
+  nazev varchar,
+  vaha integer
+);
+
+CREATE TABLE hodnoceni (
+  idhodnoceni integer PRIMARY KEY AUTOINCREMENT,
+  iduzivatel integer,
+  idprispevek integer,
+  kvalita integer,
+  delka integer,
+  zajimavost integer,
+  slovni_hodnoceni varchar
+);
+
+CREATE TABLE prispevky (
+  idprispevek integer PRIMARY KEY AUTOINCREMENT,
+  iduzivatel integer,
+  aprouval varchar,
+  filename varchar
+);
