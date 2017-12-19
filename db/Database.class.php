814d3b99072bf819f2325a59a4ac74389e303b63
qwerty <qwerty2@students.zcu.cz>
2017-12-20 00:14:26 +0100

    PDO::FETCH_ASSOC

--- db/Database.class.php
+++ db/Database.class.php
@@ -10,7 +10,7 @@
 namespace DB;
 
 
-define("FILE","sqlite:db/db.sqlite");
+define("FILE","sqlite:".$_SERVER['DOCUMENT_ROOT']."/db/db.sqlite");
 
 // tabulky
 define("UZIVATELE","uzivatele");
@@ -19,13 +19,8 @@ define("PRAVA","prava");
 define("PRISPEVKY","prispevky");
 
 
-
-
-
-
 class Database
 {
-    private $DB_URL = "sqlite:db/db.sqlite";
     private $db;
     private $temp_statement;
 
@@ -38,7 +33,7 @@ class Database
 
     private function prepare($query) {
         $this->temp_statement = $this->db->prepare($query);
-        var_dump($this->temp_statement);
+        $this->temp_statement->setFetchMode(\PDO::FETCH_ASSOC);
     }
 
     private function bind($param,$value) {
@@ -51,13 +46,16 @@ class Database
     private function fetchAll() {
         return $this->temp_statement->fetchAll();
     }
+    private function fetch() {
+        return $this->temp_statement->fetch();
+    }
 
     public function get_user($log,$pas) {
         $this->prepare( "SELECT * FROM ".UZIVATELE." WHERE login=:login AND heslo=:heslo ;" );
         $this->bind(":login",$log);
         $this->bind(":heslo",$pas);
         $this->execute();
-        return $this->fetchAll();
+        return $this->fetch();
     }
 
     public function new_user($name, $log, $pas, $mail) {
@@ -67,5 +65,33 @@ class Database
         $this->bind(":pas",$pas);
         $this->bind(":mail",$mail);
         $this->execute();
+    }
+
+    public function get_user_by_id($id) {
+        $this->prepare( "SELECT * FROM ".UZIVATELE." WHERE iduzivatel=:iduzivatel ;" );
+        $this->bind(":iduzivatel",$id);
+        $this->execute();
+        return $this->fetch();
+    }
+
+    public function get_user_by_login($login) {
+        $this->prepare( "SELECT * FROM ".UZIVATELE." WHERE login=:login ;" );
+        $this->bind(":login",$login);
+        $this->execute();
+        return $this->fetch();
+    }
+
+    public function get_user_rights($user) {
+        $this->prepare( "SELECT * FROM ".PRAVA." WHERE idprava=:idprava ;" );
+        //var_dump($user);
+        $this->bind(":idprava",$user["idprava"]);
+        $this->execute();
+        return $this->fetch();
+    }
+
+    public function get_users() {
+        $this->prepare( "SELECT * FROM ".UZIVATELE.";" );
+        $this->execute();
+        return $this->fetchAll();
     }
 }
--- tests/dbtest.php
+++ tests/dbtest.php
@@ -3,17 +3,37 @@ require_once "./../db/Database.class.php";
 
 $db = new \DB\Database();
 
+function var_print($val) {
+    echo "<pre>";
+    echo json_encode($val, JSON_PRETTY_PRINT);
+    echo "</pre>";
+    echo "<br />";
+}
+
 
 $db->new_user("qwerty","qwerty","qwerty","qwerty");
 
 $r = $db->get_user("qwerty","qwerty");
-var_dump($r);
+var_print($r);
 $r = $db->get_user("qwerty","qwertz");
-var_dump($r);
+var_print($r);
 
 
 // test unicness
 $db->new_user("qwerty","qwerty","qwerty","qwerty");
 
 $r = $db->get_user("qwerty","qwerty");
-var_dump($r);
+var_print($r);
+
+$r = $db->get_user_rights($r);
+var_print($r);
+
+$r=$db->get_users();
+var_print($r);
+
+
+
+// test rights
+
+$r=$db->get_user_rights($r[0]);
+var_print($r);
