c5430f3d0d381f89be8c0e6c7f8e14004e68d0da
qwerty <qwerty2@students.zcu.cz>
2017-12-28 23:16:43 +0100

    twig and bootstrap stub

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
         // load DB
         require_once 'db/Database.class.php';
-        $db = new Web\Database;
+        $db = new DB\Database;
 
 
-        // načtení twigu
+        // load twig
         require_once 'libs/twig/lib/Twig/Autoloader.php';
-        Twig_Autoloader::register(); // registrace autoloaderu sablon
+        Twig_Autoloader::register();
+        $loader = new Twig_Loader_Filesystem('templates');
+        $twig = new Twig_Environment($loader);
 
-        // cesta k adresari se sablonami - od tohoto souboru
-        $loader = new Twig_Loader_Filesystem('templates'); // urcim prostor sablon
-        $twig = new Twig_Environment($loader); // nactu prostredi s "nacitacem" sablon (takhle je to bez cache)
+$template_params["nadpis"] = "Seznam osob";
+// vyplnim "nejaky" obsah
+// - na ukázku je použit obsah s HTML, který může poskytnout např. WYSIWYG editor.
+$template_params["obsah"] = "<p>První odstavec <b>textu</b>.</p> <p>Druhý <i>odstavec</i> textu.</p>";
+// uzivatele
+$template_params["uzivatele"] = array(
+    array("jmeno"=>"František", "prijmeni"=>"Novotný"),
+    array("jmeno"=>"Emil", "prijmeni"=>"Opatrný"),
+    array("jmeno"=>"Běta", "prijmeni"=>"Malá")
+);
+// ovoce
+$template_params["ovoce"] = array("jablko","hruška","třešeň","švestka");
+        $header = $twig->loadTemplate("header.twig");
+        echo $header->render($template_params);
 
-        // ziskani dane sablony z nacteneho prostredi
 
-        // $template = $twig->loadTemplate('ukazkova-sablona.tpl');
