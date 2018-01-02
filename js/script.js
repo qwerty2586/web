fdb3154a9d411ca49a6617828e5d0b403345236e
qwerty <qwerty2@students.zcu.cz>
2018-01-02 02:00:40 +0100

    login register basic logic

--- js/script.js
+++ js/script.js
@@ -13,8 +13,27 @@ function log_in() {
         function (result) {
             if (result == "OK") {
                 location.href = router.index;
+            } else {
+                showMessage(result);
             }
         });
+}
+
+function showMessage(result, where = $("#message-container")) {
+    let messages = result.split("\n");
+    messages.forEach(function (line) {
+        let parts = line.split("|");
+        if (parts.length > 1) {
+            let level = parts[0];
+            let message = parts[1];
+            var element = '<div class="alert alert-';
+            element = element.concat(level.toLowerCase());
+            element = element.concat(' alert-dismissable fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>');
+            element = element.concat(message);
+            element = element.concat('</div>');
+            where.append(element);
+        }
+    });
 }
 
 function register() {
@@ -23,20 +42,29 @@ function register() {
     $("#register-send").unbind("click");
     $("#register-send").click(function () {
         // validations
-        if ($("#register-pass1").val()!=$("#register-pass2").val()) {
+        if ($("#register-pass1").val() != $("#register-pass2").val()) {
             $("#register-password2-label").html("<font color=\"red\">Dont match</font>");
-            setTimeout(function() {$("#register").modal("show");},500);
+            setTimeout(function () {
+                $("#register").modal("show");
+            }, 500);
             return;
         }
         let username = $("#register-username").val();
         let pass = $("#register-pass1").val();
         let login = $("#register-login").val();
+        let mail = $("#register-mail").val();
         let action = "register";
-        $.post(router.login, {username: username, login: login, pass: pass, action: action},
+        $.post(router.login, {username: username, login: login, pass: pass, mail:mail, action: action},
             function (result) {
                 if (result == "OK") {
+                    showMessage("SUCCESS|Registration was sucessfull");
                     $("#login-username").val(login);
                     $("#login-password").val(pass);
+                } else {
+                    setTimeout(function () {
+                        $("#register").modal("show");
+                    }, 500);
+                    showMessage(result,$("#register-message-container"));
                 }
             });
     });
--- login.php
+++ login.php
@@ -12,7 +12,81 @@ if ($_SERVER['REQUEST_METHOD']=='GET') {
     return;
 }
 
+
 //// POST
 
 
-echo "OK";
+switch ($_POST["action"]) {
+    case "login" :
+        $login = $_POST["login"];
+        $pass = $_POST["pass"];
+        $pass = hash("sha256",$pass);
+        $user = $db->get_user($login,$pass);
+        if ($user) {
+            echo "OK";
+        } else {
+            echo "DANGER|Combination of username and password dont match\n";
+        }
+        break;
+    case "register" :
+        $username = $_POST["username"];
+        $login = $_POST["login"];
+        $pass = $_POST["pass"];
+        $mail = $_POST["mail"];
+        $all_ok = true;
+
+        // username
+        if (strlen($username)<= 2) {
+            echo "DANGER|Username must be longer than 2 characters\n";
+            $all_ok =false;
+        }
+        if (strlen($username)>= 25) {
+            echo "DANGER|Username must be shorter than 25 characters\n";
+            $all_ok =false;
+        }
+
+        // login
+        if (strlen($login)<= 2) {
+            echo "DANGER|Login must be longer than 2 characters\n";
+            $all_ok =false;
+        } elseif (strlen($login)>= 25) {
+            echo "DANGER|Login must be shorter than 25 characters\n";
+            $all_ok =false;
+        } elseif ($db->get_user_by_login($login)) {
+            echo "DANGER|This login is already in use!\n";
+            $all_ok =false;
+        }
+
+        // password
+        if (strlen($pass)<= 6) {
+            echo "DANGER|Password must be longer than 6 characters\n";
+            $all_ok =false;
+        } elseif (strlen($pass)>= 25) {
+            echo "DANGER|Password must be shorter than 25 characters\n";
+            $all_ok =false;
+        }
+        if (!preg_match("#[0-9]+#", $pass)) {
+            echo "DANGER|Password must include at least one number!\n";
+            $all_ok =false;
+        }
+        if (!preg_match("#[a-zA-Z]+#", $pass)) {
+            echo "DANGER|Password must include at least one letter!\n";
+            $all_ok =false;
+        }
+
+        //mail
+        if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+            echo "DANGER|EMail is invalid\n";
+            $all_ok =false;
+        }
+
+
+        if ($all_ok) {
+            $pass = hash("sha256",$pass);
+            $db->new_user($username,$login,$pass,$mail);
+            echo "OK";
+        }
+        break;
+    default: echo "NOT YET IMPLEMENTED";
+
+}
