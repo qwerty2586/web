3b6274fe9b8da4415f1b468d20e9114c4934508a
qwerty <qwerty2@students.zcu.cz>
2018-01-07 02:28:12 +0100

    responder

---
+++ php/Responder.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qwerty
+ * Date: 07.01.2018
+ * Time: 2:03
+ */
+
+namespace Responder;
+
+
+class Responder
+{
+    private $locked=false;
+
+    public function ok() {
+        echo "ok";
+        $this->locked=true;
+    }
+
+    public function danger($message) {
+        $this->error($message);
+    }
+
+    public function error($message) {
+        $this->respond("ERROR",$message);
+    }
+
+    public function success($message) {
+        $this->respond("SUCCESS",$message);
+    }
+
+    public function info($message) {
+        $this->respond("INFO",$message);
+    }
+
+    private function respond($tag, $message)
+    {
+        if ($this->locked) return;
+        echo $tag."|".$message."\n";
+    }
+}
